8748ea1db5594aede91df16e96a3ce9613018653
Boats <48214938+OatsLaBoats@users.noreply.github.com>
2023-11-20 23:36:19 +0100

    Haskell: Refactor main

--- Haskell/src/Main.hs
+++ Haskell/src/Main.hs
@@ -5,7 +5,7 @@ import System.Exit
 import System.Directory
 import Data.List
 
-import Lexer
+import qualified Lexer
 
 main :: IO ()
 main = do
@@ -28,16 +28,25 @@ main = do
                  \         --print-all     Enables all printing functionality.\n\
                  \         --fast          Enables optimization.\n\
                  \         --only-compile  Compiles the script without running it."
-    else
-        case filter (isSuffixOf ".sigma") args of
-            [] -> raiseError "Error: No source file provided"
-            (sourceName : _) -> do
-                fileExists <- doesFileExist sourceName
-                if not fileExists 
-                then raiseError $ "Error: File \"" ++ sourceName ++ "\" doesn't exist"
-                else do
-                    source <- readFile sourceName
-                    putStrLn . show $ scan source
+        exitWith ExitSuccess
+    else return ()
+
+    let sourceFile =
+            case filter (isSuffixOf ".sigma") args of
+                [] -> ""
+                (sourceFile : _) -> sourceFile
+
+    if null sourceFile
+        then raiseError "Error: No source file provided"
+        else return ()
+
+    fileExists <- doesFileExist sourceFile
+    if not fileExists 
+        then raiseError $ "Error: File \"" ++ sourceFile ++ "\" doesn't exist"
+        else return ()
+
+    source <- readFile sourceFile
+    putStrLn . show $ Lexer.scan source
 
 raiseError :: String -> IO ()                   
 raiseError s = do

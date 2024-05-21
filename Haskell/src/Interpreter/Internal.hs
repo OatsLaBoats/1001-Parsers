c9a2bf162a260d86343def1269b41defb0d25a9d
Boats <48214938+OatsLaBoats@users.noreply.github.com>
2024-05-21 23:53:37 +0200

    Haskell: More work on binop eval

--- Haskell/src/Interpreter/Internal.hs
+++ Haskell/src/Interpreter/Internal.hs
@@ -8,6 +8,7 @@ module Interpreter.Internal
 import qualified Data.Map as Map
 import Data.Map (Map)
 import Data.Maybe
+import GHC.Float
 import Ast
 import Interpreter.Value
 
@@ -57,16 +58,91 @@ evalExpr expr env scope = case expr of
     PrimaryExpr pexpr -> evalPrimaryExpr pexpr env scope
 
 evalBinaryExpr :: Expr -> Environment -> Scope -> IO Value
-evalBinaryExpr expr env scope = case expr of
-    BinaryExpr op lhs rhs _ -> undefined
-    _ -> undefined
+evalBinaryExpr expr env scope = case op of
+    OpOr -> simpleOp BoolValue extractBool (||)
+    OpAnd -> simpleOp BoolValue extractBool (&&)
+    OpEq -> opEq
+    OpNeq -> BoolValue . not . extractBool <$> opEq
+    OpGt -> simpleOp BoolValue id gtValues
+    OpLt -> simpleOp BoolValue id ltValues
+    OpGtEq -> undefined
+    OpLtEq -> undefined
+    OpAdd -> undefined
+    OpSub -> undefined
+    OpMul -> undefined
+    OpDiv -> undefined
+    OpMod -> undefined
+    OpIndex -> undefined
+    where
+        (op, lhs', rhs') = case expr of
+            BinaryExpr op lhs rhs _ -> (op, lhs, rhs)
+            _ -> undefined
+        
+        lhs = evalExpr lhs' env scope
+        rhs = evalExpr rhs' env scope
+
+        opEq = simpleOp BoolValue id eqValues
+
+        simpleOp constructor extractor operator =
+            liftA2 (\v1 v2 -> constructor $ extractor v1 `operator` extractor v2) lhs rhs
+
+        -- NOTE: This is beyong scuffed, I'm near certain that there is an abstraction
+        --       which solves this more elegantly but I can't think of it right now.
+        ltValues v1 v2 = case v1 of
+            IntValue v1' -> case v2 of
+                IntValue v2' -> v1' < v2'
+                FloatValue v2' -> (int2Float v1') < v2'
+                _ -> undefined
+            FloatValue v1' -> case v2 of
+                FloatValue v2' -> v1' < v2'
+                IntValue v2' -> v1' < (int2Float v2')
+                _ -> undefined
+            _ -> undefined
+
+        gtValues v1 v2 = case v1 of
+            IntValue v1' -> case v2 of
+                IntValue v2' -> v1' > v2'
+                FloatValue v2' -> (int2Float v1') > v2'
+                _ -> undefined
+            FloatValue v1' -> case v2 of
+                FloatValue v2' -> v1' > v2'
+                IntValue v2' -> v1' > (int2Float v2')
+                _ -> undefined
+            _ -> undefined
+
+        eqValues v1 v2 = case v1 of
+            IntValue v1' -> case v2 of
+                IntValue v2' -> v1' == v2'
+                FloatValue v2' -> (int2Float v1') == v2'
+                _ -> undefined
+            FloatValue v1' -> case v2 of
+                IntValue v2' -> v1' == (int2Float v2')
+                FloatValue v2' -> v1' == v2'
+                _ -> undefined
+            BoolValue v1' -> v1' == (extractBool v2)
+            StringValue v1' -> v1' == (extractString v2)
+            ArrayValue v1' -> v1' == (extractArray v2)
+            NilValue -> undefined
+
 
 evalUnaryExpr :: Expr -> Environment -> Scope -> IO Value
-evalUnaryExpr expr env scope = case expr of
-    UnaryExpr op expr' _ -> case op of
-        OpNeg -> evalExpr expr' env scope >>= pure . negateValue
-        OpNot -> evalExpr expr' env scope >>= pure . notValue
-    _ -> undefined
+evalUnaryExpr expr env scope = case op of
+    OpNeg -> negateValue <$> (evalExpr expr' env scope)
+    OpNot -> notValue <$> (evalExpr expr' env scope)
+    where
+        (op, expr') = case expr of
+            UnaryExpr op expr' _ -> (op, expr')
+            _ -> undefined
+
+        negateValue value = case value of
+            IntValue v -> IntValue $ negate v
+            FloatValue v -> FloatValue $ negate v
+            _ -> undefined
+
+        notValue value = case value of
+            BoolValue v -> BoolValue $ not v
+            _ -> undefined
+
 
 evalPrimaryExpr :: PrimaryExpr -> Environment -> Scope -> IO Value
 evalPrimaryExpr expr env scope = case expr of
--- Haskell/src/Interpreter/Value.hs
+++ Haskell/src/Interpreter/Value.hs
@@ -1,7 +1,10 @@
 module Interpreter.Value 
     ( Value(..)
-    , negateValue
-    , notValue
+    , extractInt
+    , extractFloat
+    , extractBool
+    , extractString
+    , extractArray
     ) where
 
 data Value
@@ -11,15 +14,29 @@ data Value
     | StringValue String
     | ArrayValue [Value]
     | NilValue
+    deriving Eq
 
-negateValue :: Value -> Value
-negateValue value = case value of
-    IntValue v -> IntValue $ negate v
-    FloatValue v -> FloatValue $ negate v
+extractInt :: Value -> Int
+extractInt value = case value of
+    IntValue v -> v
     _ -> undefined
 
-notValue :: Value -> Value
-notValue value = case value of
-    BoolValue v -> BoolValue $ not v
+extractFloat :: Value -> Float
+extractFloat value = case value of
+    FloatValue v -> v
     _ -> undefined
 
+extractBool :: Value -> Bool
+extractBool value = case value of
+    BoolValue v -> v
+    _ -> undefined
+
+extractString :: Value -> String
+extractString value = case value of
+    StringValue v -> v
+    _ -> undefined
+
+extractArray :: Value -> [Value]
+extractArray value = case value of
+    ArrayValue v -> v
+    _ -> undefined
